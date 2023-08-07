edc8f9c1b56c83f10021722d1acf91df2b619cbc
kyoko-furuki <furukyou18@gmail.com>
2023-08-07 18:17:00 +0900

    背景設定

--- src/app/rensou/page.js
+++ src/app/rensou/page.js
@@ -1,10 +1,12 @@
 export default function Page() {
   return (
-    <body>
+    <body background="/rensou-main.jpg">
       <h1>宮崎ゼミ26期生ですか？</h1>
-      <div>
-        <img src="/rensou-main.jpg" alt="rensou-main"></img>
-      </div>
+      <button onclick="getAnswers('yes')">はい</button>
+      <button onclick="getAnswers('yes')">たぶんそう部分的にそう</button>
+      <button onclick="getAnswers('yes')">わからない</button>
+      <button onclick="getAnswers('no')">たぶん違うそうでもない</button>
+      <button onclick="getAnswers('no')">いいえ</button>
     </body>
   );
 }
